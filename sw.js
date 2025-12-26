63a096d47c525d81ccd0321f4ce6b8765a4933d5
Suraj Prakash <surajprakash1118@gmail.com>
2025-12-26 19:42:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,32 @@
-// MGD Leaderboard Service Worker v38.0
-const CACHE_NAME = 'mgd-v1';
-const assets = ['./', './index.html', './manifest.json'];
+// MGD Leaderboard Service Worker v37.1
+const CACHE_NAME = 'mgd-cache-v37';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+// Force immediate activation of the new version
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+// Clear old caches to fix launch issues
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
